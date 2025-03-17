6b1368ae4b254f50210594f5ed0eed770ea28e40
simeonchirunga <schirunga1@byupathway.edu>
2025-03-17 10:40:05 +0200

    wk3  learning activities

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Console.WriteLine("Hello World! This is the Fraction Project.");
+        Fraction fra1 = new Fraction();
+        Console.WriteLine(fra1.GetFractiontring());
+        Console.WriteLine(fra1.GetDecimalValue());
+        Fraction fra2 = new Fraction(2);
+        Console.WriteLine(fra2.GetFractiontring());
+        Console.WriteLine(fra2.GetDecimalValue());
+       
+        Fraction fra3 = new Fraction(3, 4);
+        Console.WriteLine(fra3.GetFractiontring());
+        Console.WriteLine(fra3.GetDecimalValue());
     }
+   
+  
+
 }
